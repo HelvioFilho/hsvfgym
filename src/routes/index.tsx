42e9f342c0845a15865c1b744a36b6abfc74319d
Hélvio Filho <helviosvf@gmail.com>
2023-01-24 15:57:40 -0300

    Chore: created bottom tab routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Box, useTheme } from 'native-base';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
 import { AuthRoutes } from './auth.routes';
+import { AppRoutes } from './app.routes';
 
 
 export function Routes() {
@@ -11,9 +12,9 @@ export function Routes() {
   theme.colors.background = colors.gray[700];
 
   return (
-    <Box>
-      <NavigationContainer>
-        <AuthRoutes />
+    <Box flex={1} bg='gray.700'>
+      <NavigationContainer theme={theme}>
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   );
