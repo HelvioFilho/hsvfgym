accb75f54ab4295d873ec8941f359d5e9caf3f84
Hélvio Filho <helviosvf@gmail.com>
2023-01-24 17:16:23 -0300

    Feat: created history screen

---
+++ src/screens/History.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { Heading, SectionList, Text, VStack } from 'native-base';
+import { HistoryCard } from '@components/HistoryCard';
+import { ScreenHeader } from '@components/ScreenHeader';
+
+
+export function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada frontal', 'Remada unilateral']
+    },
+    {
+      title: '27.08.22',
+      data: ['Puxada frontal']
+    }
+  ]);
+
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title='Histórico' />
+      <SectionList
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading
+            mt={10}
+            mb={3}
+            fontFamily='heading'
+            fontSize='md'
+            color='gray.200'
+          >
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+        ListEmptyComponent={() => (
+          <Text
+            color='gray.100'
+            textAlign='center'
+          >
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </VStack>
+  );
+}
